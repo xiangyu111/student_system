2a8f42a4f3ce1988217fc1617a39149cd07ff6fb
xiangyu111 <155421198+xiangyu111@users.noreply.github.com>
2025-04-13 18:48:39 +0800

    Delete backend directory

--- backend/src/main/java/com/example/learninganalysis/service/ProgressService.java
+++
@@ -1,8 +0,0 @@
-package com.example.learninganalysis.service;
-
-import java.util.Map;
-
-public interface ProgressService {
-    Map<String, Object> getProgress(String token);
-    void updateProgress(String token, Long activityId, Integer progress);
-} 
